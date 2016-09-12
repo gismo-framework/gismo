a9b1b196120526f1eb3e014151c6530f4effe53e
matthes <leuffen@continue.de>
2016-09-12 11:13:26 +0200

    NaviTree

--- src/partial/src/Partial/Page.php
+++ src/partial/src/Partial/Page.php
@@ -9,6 +9,33 @@
     namespace Gismo\Component\Partial;
 
 
+    use Gismo\Component\Di\DiContainer;
+    use Html5\Template\HtmlTemplate;
+
     class Page extends Partial {
+
+        private $mTemplate;
+
+
+        public function __construct(DiContainer $di)
+        {
+            parent::__construct($di, false);
+        }
+
+
+        public function setTemplate($filename) {
+            $this->mTemplate = $filename;
+        }
+
+
+        public function __invoke($params = [])
+        {
+            $params = parent::__invoke($params);
+            /* @var $parser HtmlTemplate */
+            $parser = $this->mDi[HtmlTemplate::class];
+
+            return $parser->renderHtmlFile($this->mTemplate, $params);
+        }
+
 
     }
